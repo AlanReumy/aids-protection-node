8816cdf6b34698e461cdd6c4dd9d98876efd5e0a
vikoala <vikoala@foxmail.com>
2022-04-10 11:42:06 +0800

    fix: upload

--- util/formatUpload.js
+++ util/formatUpload.js
@@ -2,7 +2,9 @@ const formatUpload = (ctx) => {
     const res = {
         images: ctx.host + '\\' + ctx.req.file.path
     }
-    res.images = res.images.replaceAll('\\', '/').replace('/public', '')
+    res.images = res.images
+        .replaceAll(new RegExp('\\\\', 'g'), '/')
+        .replace('/public', '')
     return res
 }
 
