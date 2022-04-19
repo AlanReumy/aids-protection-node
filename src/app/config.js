1bd6f684a514997469c07e18dc1dbd02347f45a4
vikoala <vikoala@foxmail.com>
2022-04-19 18:00:21 +0800

    refactor: 删除旧版本

--- src/app/config.js
+++ src/app/config.js
@@ -9,6 +9,21 @@ const PRIVATE_KEY = fs.readFileSync(
 )
 const PUBLIC_KEY = fs.readFileSync(path.resolve(__dirname, './keys/public.key'))
 
+const CORS_CONFIG = {
+    origin: function () {
+        //设置允许来自指定域名请求
+        return '*'
+    },
+    maxAge: 5, //指定本次预检请求的有效期，单位为秒。
+    credentials: true, //是否允许发送Cookie
+    // 设置所允许的HTTP请求方法
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+    // 设置服务器支持的所有头信息字段
+    allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    // 设置获取其他自定义字段
+    exposeHeaders: ['WWW-Authenticate', 'Server-Authorization']
+}
+
 module.exports = {
     DATABASE_HOST,
     DATABASE_DIALECT,
@@ -24,3 +39,5 @@ module.exports = {
 
 module.exports.PRIVATE_KEY = PRIVATE_KEY
 module.exports.PUBLIC_KEY = PUBLIC_KEY
+
+module.exports.CORS_CONFIG = CORS_CONFIG
--- src/app/index.js
+++ src/app/index.js
@@ -3,11 +3,18 @@ const app = new Koa()
 const bodyParser = require('koa-bodyparser')
 const useRoutes = require('../router')
 const errorHandler = require('../app/error-handle')
+const cors = require('koa2-cors')
+const { CORS_CONFIG } = require('./config')
 
 app.use(bodyParser())
 app.useRoutes = useRoutes
+// 加载路由
 app.useRoutes()
 
+// 错误处理
 app.on('error', errorHandler)
+
+//配置 cors 的中间件
+app.use(cors(CORS_CONFIG))
 
 module.exports = app
